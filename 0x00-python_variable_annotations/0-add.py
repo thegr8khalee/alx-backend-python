c61f7ea55d43e9dedd89c18584de3017be22e5b8
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-10-10 19:40:13 +0300

    mm

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,4 +3,13 @@
 
 
 def add(a: float, b: float) -> float:
+    """_summary_
+
+    Args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        float: _description_
+    """
     return a + b
