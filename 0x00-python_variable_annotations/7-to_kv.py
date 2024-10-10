3df180cfb317f7dc5ab3f469a111f82553b5ceaf
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-10-10 20:11:13 +0300

    mm

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
-"""README.md"""
+"""Complex types - string and int/float to tuple"""
 
-from typing import Union, Tuple
+from typing import Callable, Iterator, Union, Optional, List, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, Union[int, float]]:
-    """_summary_
-
-    Args:
-        k (str): _description_
-        v (Union[int, float]): _description_
-
-    Returns:
-        tuple[str, Union[int, float]]: _description_
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    return (k, v)
+    takes a string k and an int OR float v as arguments
+    returns a tuple.
+    """
+
+    return (k, v**2)
