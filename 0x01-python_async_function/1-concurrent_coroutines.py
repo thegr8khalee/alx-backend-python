dd3ec0d4fd8efcf241af39cc2e62cd028d4e7401
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-10-14 19:38:03 +0300

    mm

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""alx-backend-python"""
+
+import random
+import asyncio
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """_summary_
+
+    Args:
+        n (int): _description_
+        max_delay (int): _description_
+
+    Returns:
+        List[float]: _description_
+    """
+    temp = []
+    for i in range(n):
+        wait =  await wait_random(max_delay)
+        temp.append(wait)
+
+    return temp
